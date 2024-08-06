e5d4febd1693bf53e6cf57df103f63dde5d56d01
Jaden Isaac <jadenisaac10@gmail.com>
2024-08-07 00:26:41 +0530

    correct typos

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -85,26 +85,26 @@ const AuthForm = ({ type }:{type: string}) => {
 
     return (
         <section className='auth-form'>
-            <header className='flExample flExample-col gap-5 md:gap-8'>
-                <Link href='/' className='cursor-pointer flExample items-center gap-1'>
+            <header className='flex flex-col gap-5 md:gap-8'>
+                <Link href='/' className='cursor-pointer flex items-center gap-1'>
                         <Image 
                             src={"/icons/logo.svg"}
                             width={34}
                             height={34}
                             alt='Horizon Logo'
                         />
-                        <h1 className='tExamplet-26 font-ibm-plExample-serif font-bold tExamplet-black-1'>Horizon</h1>
+                        <h1 className='text-26 font-ibm-plExample-serif font-bold text-black-1'>Horizon</h1>
                     </Link>
 
-                    <div className='flExample flExample-col gap-1 md:gap-3'>
-                        <h1 className='tExamplet-24 lg:tExamplet-36 font-semibold tExamplet-grey-900'>
+                    <div className='flex flex-col gap-1 md:gap-3'>
+                        <h1 className='text-24 lg:text-36 font-semibold text-grey-900'>
                             {user
                                 ? 'Link Account'
                                 : type === 'sign-in'
                                     ? 'Sign In'
                                     : 'Sign Up'
                             }
-                            <p className='tExamplet-16 font-normal tExamplet-grey-600'>
+                            <p className='text-16 font-normal text-grey-600'>
                                 {user
                                     ? 'Link your account to get started'
                                     : 'Please enter your details'
